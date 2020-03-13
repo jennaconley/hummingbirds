25ca6cf0d4bbad910aaea50c3eb65ddf7c2f8834
Jenna Conley <jenna.conley@gmail.com>
2020-03-13 04:59:03 +0000

    clarified classes

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 # Importing and instantiating a SQLAlchemy object. 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -27,6 +25,7 @@ class BirdType(db.Model):
     range_notes =  db.Column(db.Text, nullable=True)
 
     birdsightings = db.relationship("BirdSighting")
+    taxon = db.relationship("Taxon")
 
     def __repr__(self):
         """provide helpful representation when printed""" 
@@ -38,20 +37,22 @@ class BirdType(db.Model):
 # 'sciName': 'Archilochus colubris',
 
 
-# class Clade(db.Model):
-#     """Table of genuses and their clade membership. Hummingbirds fall into nine main clades, the 
-#     Topazes, Hermits, Mangoes, Brilliants, Coquettes, Patagona, Mountain Gems, Bees, and Emeralds, 
-#     defining their relationship to nectar-bearing flowering plants 
-#     and the birds' continued spread into new geographic areas"""
+class Taxon(db.Model):
+    """Table of genuses and their clade membership. Hummingbirds fall into nine main clades,  
+    organized which define their relationships to nectar-bearing flowering plants 
+    and their continued spread into new geographic areas: 
+    Topazes, Hermits, Mangoes, Brilliants, Coquettes, Patagona, Mountain Gems, Bees, and Emeralds.
+    """
     
-#     __tablename__ = "clades"
+    __tablename__ = "taxa"
 
-#     genus = db.Column(db.String(64), nullable=False, primary_key=True)
-#     clade = db.Column(db.String(64), nullable=False)
+    genus = db.Column(db.String(64), nullable=False, primary_key=True)
+    clade = db.Column(db.String(64), nullable=True)
 
-    # def __repr__(self):
-        # """provide helpful representation when printed""" 
-        # return f"<Clade Object: genus is {self.genus}, clade is {self.clade}.>" 
+    def __repr__(self):
+        """provide helpful representation when printed""" 
+        return f"<Taxon Object: genus is {self.genus}, clade is {self.clade}.>" 
+
 
 
 class BirdSighting(db.Model):
@@ -62,12 +63,10 @@ class BirdSighting(db.Model):
     bird_sighting_id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
     ebird_id = db.Column(db.String(64), db.ForeignKey('birdtypes.ebird_id'), nullable=False)
     checklist_id = db.Column(db.String(64), db.ForeignKey('checklists.checklist_id'), nullable=False)
-    location_id = db.Column(db.String(64), db.ForeignKey('locations.location_id'), nullable=False)
     number_of_birds = db.Column(db.Integer, nullable=False)
 
     birdtype = db.relationship("BirdType")
     checklist = db.relationship("Checklist")
-    location = db.relationship("Location")
 
     def __repr__(self):
         """provide helpful representation when printed""" 
@@ -81,8 +80,10 @@ class Checklist(db.Model):
 
     checklist_id = db.Column(db.String(64), nullable=False, primary_key=True)
     datetime_object = db.Column(db.DateTime, nullable=False)
-    
+    location_id = db.Column(db.String(64), db.ForeignKey('locations.location_id'), nullable=False)
+
     birdsightings = db.relationship("BirdSighting")
+    location = db.relationship("Location")
 
     def __repr__(self):
         """provide helpful representation when printed""" 
@@ -97,8 +98,9 @@ class Location(db.Model):
     location_id = db.Column(db.String(64), nullable=False, primary_key=True)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+    country = db.Column(db.String(64), nullable=True)
 
-    birdsightings = db.relationship("BirdSighting")
+    checklists = db.relationship("Checklist")
 
     def __repr__(self):
         """provide helpful representation when printed""" 
