13a24d1419a2b41fc8a555489d7e1c0f3b3d5df2
Jenna Conley <jenna.conley@gmail.com>
2020-03-29 22:50:17 +0000

    added join in initial sightings query to checklist table which makes looping using that relationship a lot faster

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 import math 
 from jinja2 import StrictUndefined
+import time
+import random
 
 from model import connect_to_db, db, BirdType, BirdSighting, Checklist, Location
 # from model import Taxon
@@ -43,21 +45,38 @@ def begin_search():
 def show_sightings(ebird_id):
     """Render the template hummingbird.html"""
 
+    beginning = time.time()
+
     bird = BirdType.query.get(ebird_id)
 
-    sightings = BirdSighting.query.filter_by(ebird_id=ebird_id).all()
+    # sightings = BirdSighting.query.filter_by(ebird_id=ebird_id).all()
+    sightings = BirdSighting.query.filter_by(ebird_id=ebird_id).options(db.joinedload('checklist')).all()
+    #db.joinedload('location')??
 
+    # sightings = BirdSighting.query.join(Checklist).add_columns(BirdSighting.number_of_birds, Checklist.location_id).filter_by(BirdSighting.ebird_id=ebird_id).all()
+
+    post_sightings_time = time.time()
+    print(f"We just finished the sightings query.  It took {post_sightings_time - beginning} seconds")
 
     # locations = Location.query.all()
 
 
     loc_birdcount_dict = {}
-
     for sighting_object in sightings:
+        #print(sighting_object)
+        birdcount = sighting_object.number_of_birds
+
         checklist_object = sighting_object.checklist
-        location_object = checklist_object.location
-        loc_birdcount_dict[location_object.location_id] = loc_birdcount_dict.get(location_object.location_id, 0) + sighting_object.number_of_birds
-    
+        # location_object = checklist_object.location
+        # loc_birdcount_dict[location_object.location_id] = loc_birdcount_dict.get(location_object.location_id, 0) + sighting_object.number_of_birds
+        loc_id = checklist_object.location_id
+        loc_birdcount_dict[loc_id] = loc_birdcount_dict.get(loc_id, 0) + birdcount
+
+
+    post_birdcount_time = time.time()
+    print(f"We just finished the loc_birdcount_dict loop.  This last part took {post_birdcount_time - post_sightings_time} seconds")
+
+
     list_of_dicts = []
 
     for loc_code in loc_birdcount_dict.keys():
@@ -68,11 +87,16 @@ def show_sightings(ebird_id):
 
         list_of_dicts.append(current_dict)
 
+    post_current_dict_time = time.time()
+    print(f"We just finished the current_dict loop.  It took {post_current_dict_time - post_birdcount_time}")
+
+
     clat = list_of_dicts[0]['latitude']
     clong = list_of_dicts[0]['longitude']
-    
+
+    print(f"That whole shebang took {post_current_dict_time - beginning} seconds")
+
     return render_template("speciesmap.html", clat=clat, clong=clong, list_of_dicts=list_of_dicts, bird_object=bird)
-    # return render_template("hummingbird.html", list_of_dicts=list_of_dicts, bird_object=bird)
 
     # Link to eBird info:
     # https://ebird.org/species/ebird_id
