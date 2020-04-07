1bfa31cc3201a3240d49795dc5cd556d650953da
Jenna Conley <jenna.conley@gmail.com>
2020-04-07 21:33:40 +0000

    "Added minor comment clarifications"

--- static/nearby.js
+++ static/nearby.js
@@ -4,47 +4,42 @@
 
 
 function placeMarkers(sighting) {
-    
-    // console.log(sighting);
+    // Function to place markers which include pop-ups with sighting date
+
+    // alert(sighting);
 
     var marker = L.marker([sighting.lat, sighting.lng]).addTo(mymap);
 
     marker.bindPopup("Observed on " + sighting.obsDt.slice(0, 10));
 }
 
-    // 'locId': 'L1521686', 
-    //  'locName': '2223 Kodiak Dr NE', 
-    // 'obsDt': '2020-03-03 17:07', 
-    // 'howMany': 1,
-    // var res = str.slice(0, 3);
 
 
 
-// Success function to be called when server answers. Sends info. on to browser.
 function addSightings(results) {
+    // Success function to be called when server answers.
 
-    // console.log(results);
+    // alert(results);
 
     results.forEach(placeMarkers);
 }
 
 
 
-// Function to be called when the Event Listener's event happens.
 function informServer(evt) {
+    // Function to be called when the Form Submit Event Listener's event happens.
+
     // Prevents the default action that belongs to the event; request will not go straight to the server.
     evt.preventDefault();
 
     // alert("Inside the informServer function!");
 
-    // console.log("In the informServer function!");
-
-
-    // const button = $(evt.target);
+    // Getting the id value of the html element with the location-submit class,
+    // which has been set to the eBird ID needed for the eBird API call
     const button = $(".location-submit");
     const buttonID = button.attr("id");
 
-    console.log(buttonID);
+    // alert(buttonID);
 
     const url = "/nearby.json";
     const formData = {
@@ -57,5 +52,16 @@ function informServer(evt) {
 }
 
 
-// Event Listener
+
+// Form Submit Event Listener
 $('#nearby-form').on('submit', informServer);
+
+
+
+// Map Click Event Listener
+mymap.on('click', (evt) => {
+        $("#latitude-field").val(evt.latlng.lat); 
+        $("#longitude-field").val(evt.latlng.lng);        
+});
+
+
